29003ce46b088cdb6d4563a5af25a5c46c192c19
Muhammad Andhika Prasetya <andhikapraa@gmail.com>
2023-11-13 21:00:08 +0700

    Fix navigation bug in LeftDrawer

--- lib/widgets/left_drawer.dart
+++ lib/widgets/left_drawer.dart
@@ -56,7 +56,7 @@ class LeftDrawer extends StatelessWidget {
               leading: const Icon(Icons.home_outlined),
               title: const Text('Halaman Utama'),
               onTap: () {
-                Navigator.push(
+                Navigator.pushReplacement(
                   context,
                   MaterialPageRoute(builder: (context) => Menu()),
                 );
